54025063ac08cf8de232cb0f2ceb20fb12f31eca
Pulkit Jain <pulkitj79@users.noreply.github.com>
2026-02-08 20:55:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from sheets import read_sheet
+
+st.set_page_config(
+    page_title="IPL Auction",
+    layout="wide"
+)
+
+st.title("🏏 IPL Style Auction – Live")
+
+players_df = read_sheet("Players")
+teams_df = read_sheet("Teams")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.subheader("Players")
+    st.dataframe(players_df)
+
+with col2:
+    st.subheader("Teams")
+    st.dataframe(teams_df)
 
